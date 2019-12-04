160ed9c20cf01274b1211d807fb5abaad95e9800
Rob Lazzurs <rob@lazzurs.org>
2019-12-04 16:29:31 +0000

    Initial commit

---
+++ Formula/terragrunt.rb
@@ -0,0 +1,32 @@
+class Terragrunt < Formula
+  desc "Thin wrapper for Terraform e.g. for locking state"
+  homepage "https://github.com/gruntwork-io/terragrunt"
+  url "https://github.com/gruntwork-io/terragrunt.git",
+    :tag      => "v0.21.6",
+    :revision => "7be8eeda754fa1449265cd48356ec732c879ad90"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ec2971d6f1f3c4835263eb3d67dcbd8a52dafbc42a51f831fcd7aaf08124277f" => :catalina
+    sha256 "2e089cb9b5cfda3f9c023f91338fdb4ee2e92d770a147e8b789cdbb4efef1f00" => :mojave
+    sha256 "7c40aeeaac92a84b6fb1104621ca028c427e57112a4fccc55f6e8012d57a2fe2" => :high_sierra
+  end
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
+  depends_on "terraform" => :recommended
+  depends_on "tfenv" => :optional
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/gruntwork-io/terragrunt").install buildpath.children
+    cd "src/github.com/gruntwork-io/terragrunt" do
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-o", bin/"terragrunt", "-ldflags", "-X main.VERSION=v#{version}"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/terragrunt --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Lazzurs Tfenv
+
+## How do I install these formulae?
+`brew install lazzurs/tfenv/<formula>`
+
+Or `brew tap lazzurs/tfenv` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/lazzurs/homebrew-tfenv/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
