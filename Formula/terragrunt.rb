23bf933f80011b840bd5d0da7b48f02d420b72e4
Rob Lazzurs <rob.lazzurs@amach.ie>
2020-02-12 11:27:15 +0000

    Updating to latest version.
    This updates Terragrunt to the latest version and removing the bottle
    until I can get this working with my tap.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -2,15 +2,8 @@ class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://github.com/gruntwork-io/terragrunt"
   url "https://github.com/gruntwork-io/terragrunt.git",
-    :tag      => "v0.21.7",
-    :revision => "586f3e4516d8b65cc2a8ad41e508fabd8cd8a4e1"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "b844b8023db001c52de749b09961d3e36cf2ddf7254178dedfff87b3c1e4d0a2" => :catalina
-    sha256 "412a0f2f0723126c06ac81682575a0ede4e1343dba5c39e4711567b2a890a1ca" => :mojave
-    sha256 "7634bd2928f46b6305bd35d05f7be453ee248f0cd8d9ffb573962137eb6b3682" => :high_sierra
-  end
+    :tag      => "v0.21.13",
+    :revision => "d17db6693618cb77b2fd24c305fc323a8c5c61ba"
 
   depends_on "dep" => :build
   depends_on "go" => :build
